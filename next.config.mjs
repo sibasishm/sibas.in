9bceeead28fcae853ea919bee14d3d7f5873230c
Sibasish Mohanty <smsibasish@gmail.com>
2024-06-24 01:27:31 +0530

    fixing prod deployment issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	experimental: {
+		outputFileTracingIncludes: {
+			'/content': ['./public/**/*'],
+		},
+	},
+};
 
 export default nextConfig;
